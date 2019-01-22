c1b2138930a9685e9054a0cd9c4165eedabd3315
gonzalo <meru.vi06@gmail.com>
2019-01-22 09:41:31 -0400

    CREATED MODEL FACTURA

--- src/main/java/com/meruvia/facturation/model/Client.java
+++ src/main/java/com/meruvia/facturation/model/Client.java
@@ -1,6 +1,5 @@
 package com.meruvia.facturation.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ src/main/java/com/meruvia/facturation/model/Factura.java
@@ -0,0 +1,59 @@
+package com.meruvia.facturation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Factura {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String description;
+	
+	private String observation;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="client_id")
+	private Client client;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getObservation() {
+		return observation;
+	}
+
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
+}
